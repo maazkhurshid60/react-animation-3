12deb2a3a20727878b26f404060c7674c8c4116b
maazkhurshid60 <maaz.khurshid60@gmail.com>
2024-07-04 16:58:23 +0500

    Commiting changes for git activity

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from './App.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  //again adding changes for git activity
+  //changes this line  for git activity
   <React.StrictMode>
     <App />
   </React.StrictMode>,
