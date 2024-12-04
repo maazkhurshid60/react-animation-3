a49e62fb4d4dc688735b831fc37b593db2264285
maazkhurshid60 <maaz.khurshid60@gmail.com>
2024-12-04 13:18:30 +0500

    git activity

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import './index.css'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  //Deleted this line  for git activity
+  //Adding this line  for git activity
   <React.StrictMode>
     <App />
   </React.StrictMode>,
